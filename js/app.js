7bdc42d52de2604cf05ef9fffa8a05f192b4cf4d
amber <amberlburroughs>
2015-11-14 23:10:12 -0800

    creat plan/psuedo code for to do list application

---
+++ js/app.js
@@ -0,0 +1,39 @@
+//user interaction doesn't provide desired results
+//add user interactivity to manage daily task
+
+//add task
+  //when button clicked
+  //create task- new li with input from #newTask:
+    //input (check box)
+    //label
+    //input (text) for editing mode
+    //button.edit
+    //button.delete
+    //each element, needs modified and appended
+
+
+//edit task
+  //when the edit button is clicked
+    //if the class of parent is .editMode
+      //switch from .editMode
+      //label text becomes the input's value
+    //else
+      //switch to .editMode
+       //input value becomes the label's txt
+  //toggle .editMode on parent
+
+
+//delete task
+  //when delete button is clicked
+    //remove parent li from ul
+
+
+//complete task
+  //when checkbox is checked
+  //appened task li to #completed-tasks
+
+
+//mark as incomplete
+  //when checkbox is unchecked
+    //append to #incomplete-tasks
+
